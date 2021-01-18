4ad2feef114ba9057f2d36e1f3755b25fa0a949c
lucaandaloro <luca.andaloro@gmail.com>
2021-01-18 20:15:01 +0100

    Update modello.js

--- js/modello.js
+++ js/modello.js
@@ -1,35 +1,23 @@
-function aggiungiModello(){
-    // instantiate a loader
-    var loader = new THREE.OBJLoader();
-    // load a resource
-    loader.load(
-        // resource URL
-        'modello_3d/test4.obj',
-        // Function when resource is loaded
-        function ( object ) {
-            scene.add( object );
+var model;
+function aggiungiModello3(){
+    const stacy_mtl = new THREE.MeshPhongMaterial({
+        color: 0xffffff,
+        skinning: true
+      });
+    let loader = new THREE.GLTFLoader();
+    loader.load('modello_3d/vespa.gltf', function(gltf){
+    car = gltf.scene.children[4];
+    model  = gltf.scene;
+    scene.add(model);
+
+    car.traverse(o => {
+        if (o.isMesh) {
+          o.castShadow = true;
+          o.receiveShadow = true;
+          o.material = stacy_mtl; // Add this line
         }
-    );
-}
-
-function aggiungiModello2(){
-    var mtlLoader = new THREE.MTLLoader();
-    mtlLoader.setBaseUrl( 'modello_3d/' );
-    mtlLoader.setPath( 'modello_3d/' );
-    var url = "test4.mtl";
-    mtlLoader.load( url, function( materials ) {
-
-        materials.preload();
-
-        var objLoader = new THREE.OBJLoader();
-        objLoader.setMaterials( materials );
-        objLoader.setPath( 'modello_3d/' );
-        objLoader.load( 'test4.obj', function ( object ) {
-
-            //object.position.y = - 95;
-            scene.add( object );
-
-        }, onProgress, onError );
-
+       });
     });
+
+   
 }
