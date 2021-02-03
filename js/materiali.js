187ef10d5014278d9177bf0d26593efd6fba758e
lucaandaloro <luca.andaloro@gmail.com>
2021-02-03 15:45:09 +0100

    Update materiali.js

--- js/materiali.js
+++ js/materiali.js
@@ -20,6 +20,61 @@ function materialWithTextures(){
    
 }
 
+function getMateriale(materiale){
+    switch(materiale){
+        case "vetro":
+            var material =  new THREE.MeshPhongMaterial({
+                color: 'rgb(255,255,210)',
+                shininess: 64,
+                specular: new THREE.Color(0.31,0.31,0.31),
+                opacity: 0.9,
+                transparent: true,
+                blendSrc: THREE.SrcAlphaFactor,
+                blendDst: THREE.OneMinusSrcAlphaFactor,
+                blendEquation: THREE.AddEquation
+            });
+            break;
+        case "grass":
+             var material = new THREE.MeshPhongMaterial({
+                map: textureLoader('textures/erba.jpg'),
+                color: 0x98e070,
+                side: THREE.DoubleSide
+            });
+            break;
+    
+            case "gomma":
+            var material =  new THREE.MeshPhongMaterial({
+                color: 'rgb(38, 38, 38)',
+                side: THREE.DoubleSide
+            });
+            break;
+            
+        
+            case "mulinoPali":
+            var material =  new THREE.MeshPhongMaterial({
+                map: textureLoader('../textures/wood2.jpg'),
+                side: THREE.DoubleSide,
+                
+            });
+            break;
+            case "nuvola":
+            var material =  new THREE.MeshPhongMaterial({
+                color: "white",
+                side: THREE.DoubleSide,
+                
+            });
+            break;
+            case "foglie":
+                var material =  new THREE.MeshPhongMaterial({
+                    color: 'rgb(45,80,40)',
+                    side: THREE.DoubleSide,
+                    
+                });
+                break;
+    }
+    return material;
+}
+
 function textureLoader( file ){
     var tl = new THREE.TextureLoader();
     var newTex = tl.load( file );
