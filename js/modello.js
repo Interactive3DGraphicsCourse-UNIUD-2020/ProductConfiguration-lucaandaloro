d92ed2873f64664ccd994d83a8fed48fc78413a3
lucaandaloro <luca.andaloro@gmail.com>
2021-02-19 11:54:27 +0100

    Update modello.js

--- js/modello.js
+++ js/modello.js
@@ -3,33 +3,41 @@ function caricaModello(materiale_scocca, materiale_cerchione, material_sella){
     
   var loader = new THREE.GLTFLoader();
   loader.useIndices = true;
-    loader.load( "modello_3d/prova2.gltf", function ( model ) {
+    loader.load( "modello_3d/vespa.gltf", function ( model ) {
 
+      // Seleziono tutti le parti dell'oggetto 3D
       cerchione_ant = model.scene.children[4];
       parafango_ant = model.scene.children[5];
       pneumatico_parafango_ant = model.scene.children[6];
-      ammortizzatore_ant = model.scene.children[7];
-      logo = model.scene.children[8];
-      clacson = model.scene.children[9];
-      maschera_ant = model.scene.children[10];
-      vetrino_ant = model.scene.children[11];
-      munubrio_centrale = model.scene.children[12];
-      manubri_laterali = model.scene.children[13];
-      scocca = model.scene.children[14];
-      pneumatico_scorta = model.scene.children[15];
-      bande_pedana = model.scene.children[16];
-      freno_posteriore = model.scene.children[17];
-      sella = model.scene.children[19];
-      borse_lat = model.scene.children[21];
-      bande_laterali = model.scene.children[22];
-      pedivella_accensione = model.scene.children[23];
-      marmitta = model.scene.children[24];
-      motore = model.scene.children[25];
-      ammortizzatore_posteriore = model.scene.children[26];
-      cerchione_post = model.scene.children[27];
-      pneumatico_anteriore = model.scene.children[28];
-      fanalino_post = model.scene.children[29];
-      test = model.scene.children[21];
+      ammortizzatore_ant_asta = model.scene.children[7];
+      ammortizzatore_ant_molla = model.scene.children[8];
+      ammortizzatore_ant_attacco = model.scene.children[9];
+      logo = model.scene.children[10];
+      clacson = model.scene.children[11];
+      maschera_ant = model.scene.children[12];
+      vetrino_ant = model.scene.children[13];
+      manubrio_destro_ferro = model.scene.children[14];
+      manubrio_centrale = model.scene.children[15];
+      manopola_destra = model.scene.children[16];
+      scocca = model.scene.children[17];
+      pneumatico_scorta = model.scene.children[18];
+      bande_pedana = model.scene.children[19];
+      freno_posteriore = model.scene.children[20];
+      sella = model.scene.children[22];
+      borse_lat = model.scene.children[24];
+      bande_laterali = model.scene.children[25];
+      pedivella_accensione = model.scene.children[26];
+      marmitta = model.scene.children[27];
+      motore = model.scene.children[28];
+      ammortizzatore_posteriore = model.scene.children[29];
+      cerchione_post = model.scene.children[30];
+      pneumatico_anteriore = model.scene.children[31];
+      fanalino_post = model.scene.children[32];
+      manopola_sinistra = model.scene.children[36];
+      leva_freno_sinistro = model.scene.children[37];
+      leva_freno_destro = model.scene.children[38];
+      manubrio_sinistro_ferro = model.scene.children[39];
+      test = model.scene.children[34];
       
      
     
@@ -45,6 +53,34 @@ function caricaModello(materiale_scocca, materiale_cerchione, material_sella){
                     o.material = materiale_scocca; // Add this line
                     }
                 });
+                manubrio_sinistro_ferro.traverse(o => {
+                  if (o.isMesh) {
+                  o.castShadow = true;
+                  o.receiveShadow = true;
+                  o.material = materiale_scocca; // Add this line
+                  }
+              });
+              leva_freno_destro.traverse(o => {
+                if (o.isMesh) {
+                o.castShadow = true;
+                o.receiveShadow = true;
+                o.material = materiale_cerchione; // Add this line
+                }
+            });
+            leva_freno_sinistro.traverse(o => {
+              if (o.isMesh) {
+              o.castShadow = true;
+              o.receiveShadow = true;
+              o.material = materiale_cerchione; // Add this line
+              }
+          });
+              manubrio_destro_ferro.traverse(o => {
+                if (o.isMesh) {
+                o.castShadow = true;
+                o.receiveShadow = true;
+                o.material = materiale_scocca; // Add this line
+                }
+            });
                 
                 borse_lat.traverse(o => {
                   if (o.isMesh) {
@@ -62,7 +98,7 @@ function caricaModello(materiale_scocca, materiale_cerchione, material_sella){
                 }
             });
 
-            munubrio_centrale.traverse(o => {
+            manubrio_centrale.traverse(o => {
               if (o.isMesh) {
               o.castShadow = true;
               o.receiveShadow = true;
@@ -132,7 +168,34 @@ function caricaModello(materiale_scocca, materiale_cerchione, material_sella){
     o.material = material_sella;
     }
 });  
-  
+pedivella_accensione.traverse(o => {
+  if (o.isMesh) {
+  o.castShadow = true;
+  o.receiveShadow = true;
+  o.material = getMateriale("pedali");
+  }
+});
+freno_posteriore.traverse(o => {
+  if (o.isMesh) {
+  o.castShadow = true;
+  o.receiveShadow = true;
+  o.material = getMateriale("pedali");
+  }
+});
+manopola_sinistra.traverse(o => {
+  if (o.isMesh) {
+  o.castShadow = true;
+  o.receiveShadow = true;
+  o.material = getMateriale("manopola");
+  }
+}); 
+manopola_destra.traverse(o => {
+  if (o.isMesh) {
+  o.castShadow = true;
+  o.receiveShadow = true;
+  o.material = getMateriale("manopola");
+  }
+}); 
              
     } );
 
